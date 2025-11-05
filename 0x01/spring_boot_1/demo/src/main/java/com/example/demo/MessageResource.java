92e4f20be1090af42c6ba78ea19b832e2601e7f5
Wellington Freire <wellington.freire@live.com>
2025-11-05 20:37:39 -0300

    add project

--- 0x01/spring_boot_1/demo/src/main/java/com/example/demo/MessageResource.java
+++ 0x01/spring_boot_1/demo/src/main/java/com/example/demo/MessageResource.java
@@ -1,8 +1,6 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("messages/")
@@ -12,6 +10,19 @@ public class MessageResource {
     public String simpleMessageWelcome() {
 
         return "BEM VINDO A AULA DE MICROSSERVIÇO USANDO SPRING BOOT !!!";
+
+    }
+
+    @GetMapping("/login/{user}/{password}")
+    public String login(@PathVariable String user, @PathVariable String password) {
+        if(user == null || user.isEmpty()){
+           return  "USUÁRIO E SENHA NÃO INFORMADOS";
+        }
+        if(user.length() > 15 || password.length() > 15 ){
+            return  "USUÁRIO E SENHA INVÁLIDOS";
+        }
+
+        return "LOGIN EFETUADO COM SUCESSO !!!";
 
     }
 }
