1a1d1c017be9f24074ef9e5ea79eba4179979ac1
Wellington Freire <wellington.freire@live.com>
2025-11-15 20:25:55 -0300

    add

--- 0x03/criando_spring_boot_JPA/src/main/java/br/com/techCamps/demo/DemoApplication.java
+++ 0x03/criando_spring_boot_JPA/src/main/java/br/com/techCamps/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.techCamps.demo;
+package src.main.java.br.com.techCamps.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
