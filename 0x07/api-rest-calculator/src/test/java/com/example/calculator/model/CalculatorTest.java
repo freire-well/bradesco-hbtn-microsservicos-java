73e940ac38f9ae9110e7e15e3c16f461a6812036
Wellington Freire <wellington.freire@live.com>
2025-11-16 20:22:46 -0300

    add

---
+++ 0x07/api-rest-calculator/src/test/java/com/example/calculator/model/CalculatorTest.java
@@ -0,0 +1,53 @@
+package com.example.calculator.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    @Test
+    void sumTest() {
+        // TODO
+    }
+
+    @Test
+    public void numbersNullSumTest() {
+        // TODO
+    }
+
+    @Test
+    void subTest() {
+        // TODO
+    }
+
+    @Test
+    void divideTest() {
+        // TODO
+    }
+
+    @Test
+    public void divisionByZeroTest() {
+        // TODO
+    }
+
+    @Test
+    void factorialTest() {
+        // TODO
+    }
+
+    @Test
+    void integerToBinaryTest() {
+        // TODO
+    }
+
+    @Test
+    void integerToHexadecimalTest() {
+        // TODO
+    }
+
+    @Test
+    void calculeDayBetweenDateTest() {
+        // TODO
+    }
+}
